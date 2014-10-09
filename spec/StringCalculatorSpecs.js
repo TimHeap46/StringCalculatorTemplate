16c3a01c94f43f141a7c5e44e81cacf58896d563
TimHeap46 <tim.heap@comparethemarket.com>
2014-10-09 11:30:16 +0100

    TH javascript 2 tests working

--- spec/StringCalculatorSpecs.js
+++ spec/StringCalculatorSpecs.js
@@ -8,16 +8,11 @@ describe("String Calculator", function () {
     it('Add empty string returns "0"', function () {
         expect(stringCalculator.add(panel.utils.StringCalculator.prototype.emptyString)).toEqual("0");
     });
-
     it('Add 1 returns "1"', function () {
         expect(stringCalculator.add("1")).toEqual("1");
     });
-
-    it('returns 3 when passed 1,2', function () {
-
-
-        var param = "1,2";
-        expect(stringCalculator.add(param)).toEqual("3");
+    it('Adds 1,2 return "3', function () {
+       expect(stringCalculator.add("1,2")).toEqual("3");
     });
 
 });
--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -15,20 +15,38 @@ panel.utils.StringCalculator.prototype.add = function (listOfNumbers) {
     if (listOfNumbers.indexOf(",") == -1)
         return listOfNumbers;
 
-    var numbers = String.prototype.splitComma(listOfNumbers);
-    
-    if (numbers === this.emptyString) {
+    //var numberList = String.prototype.splitComma(listOfNumbers);
+    var numberList = listOfNumbers.split(",");
+    if (numberList === this.emptyString) {
 
         this.result = this.defaultValue;
+        return this.result;
     }
 
-    return this.result;
+    for (i = 0; i < numberList.length; i++) {
+        this.result = Number(this.result) + Number(numberList[i]);
+    }
+    return this.result.toString();
 };
 
 panel.utils.StringCalculator.prototype.emptyString = "";
 panel.utils.StringCalculator.prototype.defaultValue = "0";
 
-String.prototype.splitComma = function () {
-    return this.split(",");
-};
+//String.prototype.splitComma = function () {
+    //return this.split(",");
+    //var variables = this.split(",");
+    
+    //for(var i=0;i<variables.length;i++)
+    //{
+    //    this.result += variables[i];
+    //}
+    //return this.result;
+    //return String.prototype.split(/,\s*/);
+//};
+
+//String.prototype.splitComma = splitByComma;
+//function splitByComma() {
+//    var variables = this.split(",");
+//    return variables;
+//}
 
